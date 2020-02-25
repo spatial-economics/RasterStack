9f862a25c95787f352473e396a5549db7af6033c
Kevin Oluoch <kevin.o.oluoch@gmail.com>
2020-02-25 16:20:13 +0300

    Make it possible to select projection
    Also remove output dir; reserved for point extraction within the same project.
    And, added comment

--- code/RasterStack.R
+++ code/RasterStack.R
@@ -18,31 +18,30 @@ population.paths <- list.files("data/WorldPop", full.names = TRUE,
 bioclimatic.paths <- list.files("data/BIOclim", full.names = TRUE, 
                                 pattern = "*.tif$")
 
-output.dir <- "output"
-projexion <- "WGS" # LAEA
-
+projexions <- 
+  list(WGS =  CRS("+proj=longlat +datum=WGS84 +no_defs
+                   +ellps=WGS84 +towgs84=0,0,0"),
+       LAEA = CRS("+proj=laea +lat_0=5 +lon_0=20 +x_0=0 +y_0=0 
+                  +a=6370997 +b=6370997 +units=m +no_defs"))
+# Output Projection
+projexion <- "LAEA" # "WGS" # 
+# 
+# output.dir <- "output"
 
 # DATA PREPARATION --------------------------------------------------------
 
 # Create working sub-directiries
 dir.create("scratch/shp", showWarnings=FALSE, recursive = TRUE)
 dir.create("scratch/raster", showWarnings=FALSE, recursive = TRUE)
-dir.create(file.path(output.dir, "raster"), 
-           showWarnings=FALSE, recursive = TRUE)
+# dir.create(file.path(output.dir, "raster"), 
+#            showWarnings=FALSE, recursive = TRUE)
 # Load data
 elevation.rasters_ <- lapply(elevation.paths, raster)  # 1Km by 1Km
 bioclimatic.rasters_ <- lapply(bioclimatic.paths, raster) # 1Km by 1Km
 population.rasters_ <- lapply(population.paths, raster) # 100m by 100m
-
 # Download country shapefiles
 countrys.shp <- getCountryShp(countrys, lvl=0, path = "scratch/shp")
 
-# Propjections
-projexions <- 
-  list(WGS =  CRS("+proj=longlat +datum=WGS84 +no_defs
-                   +ellps=WGS84 +towgs84=0,0,0"),
-       LAEA = CRS("+proj=laea +lat_0=5 +lon_0=20 +x_0=0 +y_0=0 
-                  +a=6370997 +b=6370997 +units=m +no_defs"))
 
 
 # CLIP ELEVATION AND BIOCLIMATIC DATA TO COUNTRY BOUDERS ------------------
--- code/RasterStack_Functions.R
+++ code/RasterStack_Functions.R
@@ -1,9 +1,20 @@
+# Propjections
+projexions <- 
+  list(WGS =  CRS("+proj=longlat +datum=WGS84 +no_defs
+                   +ellps=WGS84 +towgs84=0,0,0"),
+       LAEA = CRS("+proj=laea +lat_0=5 +lon_0=20 +x_0=0 +y_0=0 
+                  +a=6370997 +b=6370997 +units=m +no_defs"))
+
+
 maskCountry <- function(countrys, raster.files, category, projexion) {
+  # Mask raster file to countries boundaries
   output.list <- list()
+  newproj <- toString(projexions[[projexion]])
   for (cntry in countrys) {
     i <- 0
     cntry.rasters <- list()
     for (raster.file in raster.files){
+      oldproj <- toString(crs(raster.file))
       i <- i + 1
       output.file <-
         file.path(
@@ -18,10 +29,21 @@ maskCountry <- function(countrys, raster.files, category, projexion) {
         )
       
       dir.create(dirname(output.file), showWarnings = FALSE, recursive = TRUE)
+      # Transform shapefile if it's projection is diffrent from raster's
+      if (!oldproj == toString(cntry@proj4string)) cntry <- spTransform(cntry,
+                                                              crs(raster.file))
+      # Crop extent before masking to speed mask process
       cntry.extent <- crop(raster.file, cntry, 
                            filename="scratch/tmp.tif", overwrite=TRUE)
       cntry.rasters[[i]] <- mask(cntry.extent, cntry,
                                  filename = output.file, overwrite=TRUE)
+      # Transform raster if projection is diffrent from required
+      if ( !oldproj == newproj){
+        cntry.rasters[[i]] <- projectRaster(cntry.rasters[[i]], 
+                                            crs=CRS(newproj),
+                                            filename = output.file, 
+                                            overwrite=TRUE)
+        }
     }
     output.list[[cntry$GID_0[1]]] <- cntry.rasters
     
@@ -30,9 +52,12 @@ maskCountry <- function(countrys, raster.files, category, projexion) {
 }
 
 aggregatePopRaster <- function(raster.files, category, projexion) {
+  # Aggregate a raster files from a list 
   output.list <- list()
+  newproj <- toString(projexions[[projexion]])
   i <- 0
   for (raster.file in raster.files){
+    oldproj <- toString(crs(raster.file))
     i <- i + 1
     output.file <-
       file.path(
@@ -46,13 +71,21 @@ aggregatePopRaster <- function(raster.files, category, projexion) {
                ".tif")
       )
     dir.create(dirname(output.file), showWarnings = FALSE, recursive = TRUE)
-    output.list[[i]] <- aggregate(raster.file, fact=10, fun=sum, expand=TRUE, 
-                                  na.rm=TRUE, filename=output.file, overwrite=TRUE)
+    output.list[[i]] <- aggregate(raster.file, fact=10, fun=sum, 
+                                  expand=TRUE, na.rm=TRUE, filename=output.file,
+                                  overwrite=TRUE)
+    
+    # Transform aggregated raster if projection is diffrent from required
+    if (!oldproj == newproj){
+      output.list[[i]] <- projectRaster(output.list[[i]], crs=CRS(newproj),
+                                        filename = output.file, overwrite=TRUE)
+    }
   }
   output.list
 }
 
 getCountryShp <- function(cntrys.vector, lvl, path) {
+  # Get country shapefiles from GADM
   output.list <- list()
   for (cntry in cntrys.vector) {
     output.list[[cntry]] <- getData("GADM", country=cntry, level=lvl, path=path)
